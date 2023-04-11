4f2e02704e172a50626363b7a49dc0680806263c
Lee SeongHun <1230rjdgns@gmail.com>
2023-04-11 21:23:14 +0900

    브랜치 합침

---
+++ app/src/main/java/com/capstone/affinity_ad/ProductActivity.java
@@ -0,0 +1,40 @@
+package com.capstone.affinity_ad;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class ProductActivity extends AppCompatActivity {
+
+    private static final String TAG = "Main_Activity";
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private FragmentAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+
+        tabLayout=findViewById(R.id.tabLayout);
+        viewPager=findViewById(R.id.view_pager);
+        adapter=new FragmentAdapter(getSupportFragmentManager(),1);
+
+        //FragmentAdapter에 컬렉션 담기
+        adapter.addFragment(new ProductDetailFragment());
+        adapter.addFragment(new ProductCommunityFragment());
+
+        //ViewPager Fragment 연결
+        viewPager.setAdapter(adapter);
+
+        //ViewPager과 TabLayout 연결
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setText("상세정보");
+        tabLayout.getTabAt(1).setText("커뮤니티");
+    }
+}
